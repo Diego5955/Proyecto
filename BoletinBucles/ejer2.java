d2e36745172dd3167886a99567608c7395098633
Diego5955 <diroso@xuqueralumnat.es>
2020-11-04 11:09:16 +0100

    first commit

---
+++ BoletinBucles/ejer2.java
@@ -0,0 +1,14 @@
+package BoletinBucles;
+
+import java.util.Scanner;
+
+public class ejer2 {
+
+	public static void main(String[] args) {
+Scanner teclado = new Scanner(System.in);
+		
+		int numero=0;
+		
+	}
+
+}
