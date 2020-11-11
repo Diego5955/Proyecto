8bec6282486914dda0e3745bc3005570cfd0dab0
Diego5955 <diroso@xuqueralumnat.es>
2020-11-11 10:43:03 +0100

    first commit

---
+++ P_380_MeCaso.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Diego Y Joaquín
+ *
+ */
+public class P_380_MeCaso {
+
+	/**
+	 * @param 
+	 * Entrada : Cada caso de prueba comienza con un número n indicando la
+	 *                cantidad de gastos que se sufren al organizar una boda (como
+	 *                mucho 50.000). A continuación aparecen, en otra línea, n
+	 *                números positivos con todos esos gastos. La entrada termina
+	 *                con una boda sin gastos (obviamente imposible) que no debe
+	 *                procesarse.
+	 * 
+	 * Salida : Para cada caso de prueba se escribirá, en una línea
+	 *            	  independiente, el coste total de la boda. Todo está por las
+	 *                nubes, pero la suma de los gastos será menor que 109.
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner teclado = new Scanner(System.in);
+
+		int num = teclado.nextInt();
+		int suma = 0;
+		
+		do {
+			
+			
+			
+			
+			
+			
+		} while (num != 0);
+			
+		
+		
+		teclado.close();
+	}
+
+}
