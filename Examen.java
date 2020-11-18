52b5859c40e5b4b5337a1791a1e937bbfb6ca117
Diego5955 <diroso@xuqueralumnat.es>
2020-11-18 08:53:15 +0100

    first commit

---
+++ Examen.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Diego Roig Solis
+ *
+ */
+public class Examen {
+
+	/**
+	 * @param P362_Navidad
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner teclado = new Scanner(System.in);
+		
+		int N = teclado.nextInt();
+		int DD;
+		int MM;
+		
+		
+		for (int cont = 0; cont < N; cont++) {
+			
+		}
+		
+		
+		teclado.close();
+	}
+
+}
